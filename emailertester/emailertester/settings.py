829b158b2cd417053c50f32de3a69d504b118c77
moemen <moemenology@gmail.com>
2013-02-14 15:01:32 +0200

    change the test app with Django 1.4 app

---
+++ emailertester/emailertester/settings.py
@@ -0,0 +1,200 @@
+import os, sys
+def rel(*x):
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+
+sys.path.insert(0, rel('../..'))
+
+SITE_ID = 1
+
+ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
+CURRENT_PATH = os.getcwd()
+
+DEBUG = True
+LOCAL = DEBUG
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Test User', 'test@gmail.com'),
+)
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(CURRENT_PATH, 'database.sqlite')
+    }
+}
+
+if DEBUG:
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = 'moemenology@gmail.com'
+    EMAIL_HOST_PASSWORD = '3xpl0!t000'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    
+
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# On Unix systems, a value of None will cause Django to use the same
+# timezone as the operating system.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'Africa/Cairo'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale.
+USE_L10N = True
+
+# If you set this to False, Django will not use timezone-aware datetimes.
+USE_TZ = False
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = "%s/%s/" % (ROOT_PATH, 'media')
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = '/media/'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = ''
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'g5w+_dy^z)njrm8y@-eibu-s#i@q)hajwf@k%qucm@)!xtb3nb'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    # Uncomment the next line for simple clickjacking protection:
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'emailertester.urls'
+
+# Python dotted path to the WSGI application used by Django's runserver.
+WSGI_APPLICATION = 'emailertester.wsgi.application'
+
+TEMPLATE_DIRS = (
+    "%s/%s/" % (ROOT_PATH, 'templates'),
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'tinymce',
+    'emailer',
+    'south',
+)
+
+#TINYMCE_SPELLCHECKER = False
+TINYMCE_JS_URL = "%sjs/tiny_mce/tiny_mce_src.js" % MEDIA_URL
+TINYMCE_COMPRESSOR = False
+USE_COMPRESSOR = False
+TINYMCE_USE_TEMPLATES = False
+
+TINYMCE_DEFAULT_CONFIG = {
+    'theme' : "advanced",
+    'plugins' : "autolink,lists,spellchecker,pagebreak,style,layer,table,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+    
+    'document_base_url' : "/",
+    'relative_urls' : False,
+    'convert_urls': False,
+    'strict_loading_mode': 1,
+    
+    #Theme options
+    'theme_advanced_buttons1' : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
+    'theme_advanced_buttons2' : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+    'theme_advanced_buttons3' : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+    'theme_advanced_buttons4' : "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak,|,insertfile,insertimage",
+    'theme_advanced_toolbar_location' : "top",
+    'theme_advanced_toolbar_align' : "left",
+    'theme_advanced_statusbar_location' : "bottom",
+    'theme_advanced_resizing' : False,
+
+    #Skin options
+    'skin' : "o2k7",
+    'skin_variant' : "silver",
+    
+    'template_external_list_url' : "emailer/templates/",
+
+}
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
--- emailertester/emailertester/urls.py
+++ emailertester/emailertester/urls.py
@@ -1,35 +1,33 @@
+from django.conf.urls import patterns, include, url
 from django.conf import settings
-from django.conf.urls.defaults import *
 from django.contrib import admin
 import os
 import django
 
+from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    
+
     #emailer
     url(r'^emailer/', include('emailer.urls')),
     
     #tinymce
     url(r'^tinymce/', include('tinymce.urls')),
-    
-    #admin
-    #url(r'^admin/(.*)', admin.site.root),
-    url(r'^admin/', include(admin.site.urls)),
-    
     url(r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:], 
         'django.views.static.serve', {"document_root": settings.MEDIA_ROOT}),
-)
 
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
 
 if settings.LOCAL:
     urlpatterns += patterns('',
         (r'^%s(?P<path>.*)$'%settings.MEDIA_URL[1:], 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-        (r'^(?P<path>.*xml)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT	+	'xml', 'show_indexes': True}),
-        (r'^(?P<path>.*swf)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT	+	'flash', 'show_indexes': True}),
-        (r'^(?P<path>.*jpg)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT	+	'images', 'show_indexes': True}),
-	)
+        (r'^(?P<path>.*xml)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT   +   'xml', 'show_indexes': True}),
+        (r'^(?P<path>.*swf)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT   +   'flash', 'show_indexes': True}),
+        (r'^(?P<path>.*jpg)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT   +   'images', 'show_indexes': True}),
+    )
 
 
     admin_media_url = settings.STATIC_URL.lstrip('/') + '(?P<path>.*)$'
