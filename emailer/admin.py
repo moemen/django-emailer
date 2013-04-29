2de9912de545f170201c8e9f1c05909e5dda57d1
moemen <moemenology@gmail.com>
2013-04-29 20:02:45 +0200

    use celary and redis to send emails

--- emailer/admin.py
+++ emailer/admin.py
@@ -5,25 +5,28 @@ try:
     from tinymce.widgets import TinyMCE as HtmlWidget
 except:
     from django.forms import Textarea as HtmlWidget
-    
+
 from emailer.models import *
 # from emailer.models.Email import STATUS_PREPARED, STATUS_SENT, STATUS_ERROR
 
+
 def send_email(modeladmin, request, queryset):
-    not_sent = list(queryset.filter(status = Email.STATUS_PREPARED))
+    not_sent = list(queryset.filter(status=Email.STATUS_PREPARED))
     sent = list(queryset.filter(status=Email.STATUS_SENT))
     for email in not_sent:
         email.send()
-    sent_num = queryset.filter(status = Email.STATUS_SENT).count() - len(sent)
+    sent_num = queryset.filter(status=Email.STATUS_SENT).count() - len(sent)
     selected = queryset.all().count()
     if not_sent:
         messages.add_message(request, messages.SUCCESS, '%d have been sent from %d selected email.' % (sent_num, selected))
+
 
 class EmailAdmin(admin.ModelAdmin):
     list_display = ('email_blast', 'to_address', 'status', 'opened', 'merge_data',)
     list_filter = ('status',)
 
     actions = [send_email]
+
 
 def prepare_blast(modeladmin, request, queryset):
     not_prepared = list(queryset.filter(is_prepared=False))
@@ -32,18 +35,20 @@ def prepare_blast(modeladmin, request, queryset):
         blast.send(just_prepare=True)
 
     if not_prepared:
-        messages.add_message(request, messages.SUCCESS, ', '.join(["[%s]"%blast.name for blast in not_prepared])+' have been prepared to send.')
+        messages.add_message(request, messages.SUCCESS, ', '.join(["[%s]" % blast.name for blast in not_prepared])+' have been prepared to send.')
     if prepared:
-        messages.add_message(request, messages.WARNING, ', '.join(["[%s]"%blast.name for blast in prepared])+' have already been prepared to send.')
+        messages.add_message(request, messages.WARNING, ', '.join(["[%s]" % blast.name for blast in prepared])+' have already been prepared to send.')
 
 prepare_blast.short_description = 'Prepare for Send'
 
-class EmailBlastAdminForm(forms.ModelForm):  
+
+class EmailBlastAdminForm(forms.ModelForm):
     html = forms.CharField(widget=HtmlWidget(attrs={'cols': 80, 'rows': 40}))
-    
+
     class Meta:
         model = EmailBlast
-           
+
+
 class EmailBlastAdmin(admin.ModelAdmin):
     list_display = ('name', 'lists_str', 'is_prepared', 'send_after',)
     list_filter = ('send_after',)
@@ -58,33 +63,30 @@ class EmailBlastAdmin(admin.ModelAdmin):
                 ('send_after',),
                 ('lists',),
                 ('html',)
-                ),
+            ),
         }),
 #        ('Advanced options', {
 ##            'classes': ('collapse',),
 ##            'fields': ('enable_comments', 'registration_required', 'template_name')
 #        }),
-        )
+    )
 
-    def save_model(self, request, obj, form, change):
-        obj.save()
-        form.save_m2m()
-        if not obj.is_prepared:
-            obj.send(   )
-    
+
 class EmailTemplateAdminForm(forms.ModelForm):
     name = forms.CharField()
-    description = forms.CharField(required=False,widget=forms.Textarea(attrs={'cols': 80, 'rows': 4}))    
+    description = forms.CharField(required=False, widget=forms.Textarea(attrs={'cols': 80, 'rows': 4}))
     html = forms.CharField(widget=HtmlWidget(attrs={'cols': 80, 'rows': 40}))
-                           
-class EmailTemplateAdmin(admin.ModelAdmin):                           
+
+
+class EmailTemplateAdmin(admin.ModelAdmin):
     list_display = ('name', 'date_created',)
     form = EmailTemplateAdminForm
+
 
 class EmailListAdmin(admin.ModelAdmin):
     list_display = ('name', 'type', 'date_created', 'preview_emails', 'merge_fields',)
     list_filter = ('type', 'is_oneoff',)
-        
+
     def changelist_view(self, request, extra_context=None):
         '''
         This override, by default hides, the one off lists that are
@@ -96,9 +98,8 @@ class EmailListAdmin(admin.ModelAdmin):
             q['is_oneoff__exact'] = '0'
             request.GET = q
             request.META['QUERY_STRING'] = request.GET.urlencode()
-        return super(EmailListAdmin,self).changelist_view(request, extra_context=extra_context)
-    
-    
+        return super(EmailListAdmin, self).changelist_view(request, extra_context=extra_context)
+
 admin.site.register(EmailTemplate, EmailTemplateAdmin)
 admin.site.register(Email, EmailAdmin)
 admin.site.register(EmailBlast, EmailBlastAdmin)
--- emailer/models.py
+++ emailer/models.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import User, Group
 from django.db import connection
 from django.core.mail import EmailMultiAlternatives
 from django.template import Context, Template
-from django.conf import settings
 from django.contrib.sites.models import Site
 from django.utils.timezone import now
 from south.modelsinspector import add_introspection_rules
@@ -15,31 +14,38 @@ add_introspection_rules([], ["^emailer\.fields\.DictionaryField"])
 from emailer.html2text import html2text
 from emailer.fields import DictionaryField
 
-import uuid, json, datetime
+from celery import task
+import redis
+
+import uuid
+
 
 def make_uuid():
     return str(uuid.uuid4())
 
+
 class DefaultModel(models.Model):
     id = models.CharField(max_length=36, primary_key=True, default=make_uuid, editable=False)
-    
+
     date_created = models.DateTimeField('date created', auto_now_add=True)
     date_changed = models.DateTimeField('date changed', auto_now=True)
 
     class Meta:
         abstract = True
-  
+
+
 class EmailTemplate(DefaultModel):
     name = models.CharField(blank=False, max_length=40)
     description = models.TextField(blank=True)
     html = models.TextField(blank=False)
-    
+
     def __unicode__(self):
         return str(self.name)
-    
+
     @models.permalink
     def get_absolute_url(self):
         return ('emailer-template', (), {'template_id': self.id})
+
 
 class EmailListManager(models.Manager):
 
@@ -48,37 +54,38 @@ class EmailListManager(models.Manager):
     #def get_query_set(self):
        # return super(EmailListManager, self).get_query_set().exclude(is_oneoff=True)
 
+
 class EmailList(DefaultModel):
     LISTTYPE_SITEUSERS_USERDEFINED = 0
     LISTTYPE_QUERY_CUSTOM_SQL = 1
     LISTTYPE_RAW_EMAILS = 2
     LISTTYPE_RAW_JSON = 3
     LISTTYPE_SITEGROUPS = 4
-    
+
     EMAIL_LIST_TYPE_CHOICES = (
-                         (LISTTYPE_SITEUSERS_USERDEFINED,'Site Users - User Defined'),
-                         (LISTTYPE_QUERY_CUSTOM_SQL, 'Custom SQL Query'),
-                         (LISTTYPE_RAW_EMAILS, 'Raw Emails'),
-                         (LISTTYPE_RAW_EMAILS, 'Raw JSON'),
-                         (LISTTYPE_SITEGROUPS, 'Site Groups'),
-                         )
-    
+        (LISTTYPE_SITEUSERS_USERDEFINED, 'Site Users - User Defined'),
+        (LISTTYPE_QUERY_CUSTOM_SQL, 'Custom SQL Query'),
+        (LISTTYPE_RAW_EMAILS, 'Raw Emails'),
+        (LISTTYPE_RAW_EMAILS, 'Raw JSON'),
+        (LISTTYPE_SITEGROUPS, 'Site Groups'),
+    )
+
     class RawEmail():
         def __init__(self, email):
             self.email = email
-    
+
     name = models.CharField(blank=False, unique=True, max_length=40)
     type = models.IntegerField(
-                               blank=False,
-                               choices=EMAIL_LIST_TYPE_CHOICES, 
-                               default=LISTTYPE_SITEUSERS_USERDEFINED
-                               )
+        blank=False,
+        choices=EMAIL_LIST_TYPE_CHOICES,
+        default=LISTTYPE_SITEUSERS_USERDEFINED
+    )
     data_raw_emails = models.TextField(blank=True)
     data_site_users = models.ManyToManyField(User, blank=True)
     data_query_sql = models.TextField(blank=True)
     data_raw_json = models.TextField(blank=True)
     data_site_groups = models.ManyToManyField(Group, blank=True)
-    
+
     is_oneoff = models.BooleanField(default=False)
 
     objects = EmailListManager()
@@ -88,7 +95,7 @@ class EmailList(DefaultModel):
 
     def _is_valid_field(self, field):
         return (not field == 'id') and (not field.startswith('_'))
-    
+
     def _get_user_objs(self, users):
         class TempUser(object):
             pass
@@ -97,48 +104,48 @@ class EmailList(DefaultModel):
             tempusers = []
             for u in users:
                 t = TempUser.new()
-                for field,value in u.get_profile().__dict__.iteritems():
+                for field, value in u.get_profile().__dict__.iteritems():
                     if self._is_valid_field(field):
                         setattr(t, field, value)
                 tempusers.append(t)
-        except Exception, e:
+        except Exception:
             logger.debug("No Auth Profile Defined")
-            
+
             #eat this if there is no profile defined in settings.py
             pass
         return users
-    
+
     def get_objects(self):
-       
+
         logger.debug("Getting objects for EmailList.type=%s", self.type)
         if self.type in (EmailList.LISTTYPE_SITEUSERS_USERDEFINED,):
             users = self._get_user_objs(self.data_site_users.all())
             return users
-        
+
         elif self.type == EmailList.LISTTYPE_SITEGROUPS:
             users = []
             groups = self.data_site_groups.all()
-            
+
             for group in groups:
                 users += self._get_user_objs(group.user_set.all())
-                
+
             return users
         elif self.type in (EmailList.LISTTYPE_QUERY_CUSTOM_SQL,):
-           cursor = connection.cursor()
-           cursor.execute(self.data_query_sql)
-           rows = cursor.fetchall()
-           return [EmailList.RawEmail(row[0]) for row in rows]        
-        
+            cursor = connection.cursor()
+            cursor.execute(self.data_query_sql)
+            rows = cursor.fetchall()
+            return [EmailList.RawEmail(row[0]) for row in rows]
+
         elif self.type in (EmailList.LISTTYPE_RAW_EMAILS,):
             return [EmailList.RawEmail(email.strip()) for email in self.data_raw_emails.split(',')]
-        
+
 #        elif self.type in (EmailList.LISTTYPE_RAW_JSON,):
 #            json.loads(self.data_raw_json)
 #            return []
-        
+
         else:
             raise NotImplementedError()
-    
+
     def preview_emails(self):
         try:
             objs = self.get_objects()[:3]
@@ -148,35 +155,38 @@ class EmailList(DefaultModel):
         return preview if preview else u'(No emails found)'
     preview_emails.short_description = 'Preview Emails'
     preview_emails.allow_tags = True
-    
+
     def merge_fields(self):
         try:
             obj = self.get_objects()[0]
         except:
             obj = EmailList.RawEmail("")
-            
+
         return obj.__dict__.keys()
     merge_fields.short_description = 'Merge Fields'
     merge_fields.allow_tags = True
-    
+
     def save(self, *args, **kwargs):
         if self.is_oneoff and not self.name.startswith('_'):
             self.name = '_'+self.name
-        models.Model.save(self,*args, **kwargs)        
-         
+        models.Model.save(self, *args, **kwargs)
+
     def __unicode__(self):
         return str(self.name)
 
-class EmailBlast(DefaultModel):    
+
+class EmailBlast(DefaultModel):
     name = models.CharField(blank=False, unique=False, max_length=50)
     lists = models.ManyToManyField(EmailList)
 
     is_prepared = models.BooleanField(default=False)
-    send_after = models.DateTimeField(blank=False,
-        help_text='This is the date the email is going to be sent after. The email also must be prepared before it is sent.')
+    send_after = models.DateTimeField(
+        blank=False,
+        help_text='This is the date the email is going to be sent after. The email also must be prepared before it is sent.'
+    )
     from_address = models.EmailField(blank=False)
     subject = models.CharField(blank=False, max_length=40)
-    
+
     html = models.TextField(blank=False)
 
     class Meta:
@@ -184,20 +194,26 @@ class EmailBlast(DefaultModel):
 
     def __unicode__(self):
         return str(self.name)
-    
+
+    @task
     def _prepare_for_send(self):
         if not self.is_prepared:
-            for list in self.lists.all():
-                for obj in list.get_objects():
-                    email = Email()
-                    email.email_blast = self
-                    email.to_address = obj.email
-                    email.merge_data = obj.__dict__
-                    email.status = Email.STATUS_PREPARED
-                    email.save()
+            r = redis.StrictRedis(host='localhost', port=6379, db=0)
+            pipe = r.pipeline()
+            for mail_list in self.lists.all():
+                for obj in mail_list.get_objects():
+
+                    email = {
+                        'email_blast': self.id,
+                        'to_address': obj.email,
+                        'merge_data': obj.__dict__,
+                        'status': Email.STATUS_PREPARED,
+                    }
+                    pipe.rpush('emails', email)
+            pipe.execute()
             self.is_prepared = True
             self.save()
-                    
+
     def send(self, just_prepare=False):
         '''
         Sends an email for all objects in the assigned lists.
@@ -215,95 +231,100 @@ class EmailBlast(DefaultModel):
     lists_str.short_description = 'Lists'
     lists_str.allow_tags = True
 
+
 def _apply_merge_data(html, merge_data):
     t = Template(html)
     c = Context(merge_data)
     return t.render(c)
 
+
 class IncorrectEmailStatus(Exception):
     def __init__(self):
         Exception.__init__(self)
-                  
+
+
 class EmailManager(models.Manager):
-    
+
     def email_from_tracking(self, id):
         return self.get(id=id)
+
 
 class Email(DefaultModel):
     STATUS_PREPARED = 0
     STATUS_SENT = 1
     STATUS_ERROR = 2
-    
+
     STATUS_CHOICES = (
-                     (STATUS_PREPARED, 'Prepared'),
-                     (STATUS_SENT, 'Sent'),
-                     (STATUS_ERROR, 'Error'),
-                     )
-    
+        (STATUS_PREPARED, 'Prepared'),
+        (STATUS_SENT, 'Sent'),
+        (STATUS_ERROR, 'Error'),
+    )
+
     email_blast = models.ForeignKey(EmailBlast, blank=False)
     to_address = models.EmailField(blank=False)
     merge_data = DictionaryField(editable=False, blank=True)
-    
+
     status = models.IntegerField(blank=False, choices=STATUS_CHOICES, default=STATUS_PREPARED, editable=False)
     status_message = models.TextField(blank=True, editable=False)
     opened = models.BooleanField(default=False, editable=False)
-    
+
     # objects = EmailManager()
-    
-    def _subject(self): return self.email_blast.subject
+
+    def _subject(self):
+        return self.email_blast.subject
     subject = property(_subject)
-    
-    def _from_address(self): return self.email_blast.from_address
+
+    def _from_address(self):
+        return self.email_blast.from_address
     from_address = property(_from_address)
-    
-    def _html(self): return self.email_blast.html
+
+    def _html(self):
+        return self.email_blast.html
     html = property(_html)
-    
+
     class Meta:
         ordering = ['-date_created']
-    
+
     @models.permalink
     def get_tracking_png_url(self):
         return ('emailer-tracking_png', (), {'tracking_id': self.id})
-    
-    def _append_tracking_image(self, html):    
+
+    def _append_tracking_image(self, html):
         tracking_url = 'http://%s%s' % (Site.objects.get_current().domain, self.get_tracking_png_url())
-        
-        html = html + r'<img src="%s" alt="tracking url" id="trackingurl" />' %str(tracking_url)
+
+        html = html + r'<img src="%s" alt="tracking url" id="trackingurl" />' % str(tracking_url)
         return html
-    
+
     def _add_tracking_info(self, html):
         tracking_html = self._append_tracking_image(html)
-        
         return tracking_html
 
     def _convert_relative_urls(self, html):
         return html
-    
+
     def _prepare_html(self):
         html = _apply_merge_data(self.html, self.merge_data)
         html = self._convert_relative_urls(html)
-        
         return html
-    
+
     def _build_message(self):
         blast = self.email_blast
-        
+
         subject = blast.subject
         from_email = blast.from_address
         to = self.to_address
-        
+
         fixed_html = self._prepare_html()
-        
+
         text_content = html2text(fixed_html)
         # html_content = self._add_tracking_info(fixed_html) # I think it's not ethic!
         html_content = fixed_html
-        
+
         msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
         msg.attach_alternative(html_content, "text/html")
-    
+
         return msg
-    
+
     def send(self):
         '''
         Actually send the email using django email. If the associated blasts
